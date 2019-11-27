df91de35c243305411de1c64bf65df9c12fab1cb
Nick Teff <nicholas.teff@gmail.com>
2019-11-26 21:41:48 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ from frontend import index, app1, app2
 from waitress import serve
 
 
-debug = True
+debug = False
 
 if __name__ == '__main__':
     app1.app.enable_dev_tools(debug=False)
@@ -14,4 +14,3 @@ if __name__ == '__main__':
         server.run(host='0.0.0.0', port=8080, debug=debug)
     else:
         serve(server, host='0.0.0.0', port=8080)
-
